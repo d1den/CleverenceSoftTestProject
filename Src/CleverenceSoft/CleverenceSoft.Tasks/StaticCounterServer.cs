9b63196607705c96decf501d3f24c98b2e5940d9
Dmitry Denisov <67658531+d1den@users.noreply.github.com>
2022-10-01 11:59:06 +0300

    Добавлен проект и реализован "Сервер" счётчик с синхронизацией потоков (#1)
    Co-authored-by: Dmitriy Denisov <ddenisov@navicons.com>

---
+++ Src/CleverenceSoft/CleverenceSoft.Tasks/StaticCounterServer.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading;
+
+namespace CleverenceSoft.Tasks
+{
+    /// <summary>
+    /// Статический класс "Сервер", предоставляющий сервис счётчика с синхронизацией потоков
+    /// </summary>
+    public static class StaticCounterServer
+    {
+        /// <summary>
+        /// Примитив синхронизации потоков
+        /// </summary>
+        private static readonly ReaderWriterLockSlim _Lock = new ReaderWriterLockSlim();
+
+        /// <summary>
+        /// Поле значения счётчика
+        /// </summary>
+        private static int _Count = 0;
+
+        /// <summary>
+        /// Получить значение счётчика
+        /// </summary>
+        /// <returns>Значение счётчика</returns>
+        public static int GetCount()
+        {
+            _Lock.EnterReadLock();
+            try
+            {
+                return _Count;
+            }
+            finally
+            {
+                _Lock.ExitReadLock();
+            }
+        }
+
+        /// <summary>
+        /// Добавить значение к счётчику с монопольной блокировкой доступа
+        /// </summary>
+        /// <param name="value">Добавляемое значение</param>
+        public static void AddToCount(int value)
+        {
+            _Lock.EnterWriteLock();
+            try
+            {
+                _Count += value;
+            }
+            finally
+            {
+                _Lock.ExitWriteLock();
+            }
+        }
+    }
+}
